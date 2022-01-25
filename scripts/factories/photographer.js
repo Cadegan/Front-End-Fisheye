aea76c179950562ef79d1028c8315eecfa1d0e59
Jean-Charles MAURICE <cadegancode@gmail.com>
2022-01-25 12:38:02 +0900

    Factories photographer initialisation constantes

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tag, price} = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +9,18 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const h3 = document.createElement('h3');
+        h3.textContent = `${city}, ${country}`;
+        const quote = document.createElement('p');
+        quote.textContent = tag;
+        const priceDay = document.createElement('p');
+        priceDay.textContent = price;
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(quote);
+        article.appendChild(priceDay);
         return (article);
     }
     return { name, picture, getUserCardDOM }
