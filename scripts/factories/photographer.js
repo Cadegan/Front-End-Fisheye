fc026e20208d29a104d98bc33e74ed3279c9b12a
Jean-Charles MAURICE <cadegancode@gmail.com>
2022-01-25 12:50:02 +0900

    réécritures simples

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,18 +1,23 @@
 function photographerFactory(data) {
-    const { name, portrait, city, country, tag, price} = data;
+    const {name, city, country, tagline, price, portrait} = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
         const h3 = document.createElement('h3');
         h3.textContent = `${city}, ${country}`;
+
         const quote = document.createElement('p');
-        quote.textContent = tag;
+        quote.textContent = tagline;
+
         const priceDay = document.createElement('p');
         priceDay.textContent = price;
 
