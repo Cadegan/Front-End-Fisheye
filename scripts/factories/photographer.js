899cc7988105515ed40c484dde199b9228487e58
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-01-26 12:22:04 +0900

    Affichage portraits accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,32 +1,31 @@
 function photographerFactory(data) {
-    const {name, city, country, tagline, price, portrait} = data;
-
+    const { name, city, country, tagline, price, portrait, id } = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
+        const article = document.createElement('article');
 
-        const img = document.createElement( 'img' );
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
 
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
 
-        const h3 = document.createElement('h3');
-        h3.textContent = `${city}, ${country}`;
+        const location = document.createElement('h3');
+        location.textContent = `${city}, ${country}`;
 
         const quote = document.createElement('p');
         quote.textContent = tagline;
 
         const priceDay = document.createElement('p');
-        priceDay.textContent = price;
+        priceDay.textContent = price + "€";
 
         article.appendChild(img);
         article.appendChild(h2);
-        article.appendChild(h3);
+        article.appendChild(location);
         article.appendChild(quote);
         article.appendChild(priceDay);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, location, tagline, price, getUserCardDOM }
 }
