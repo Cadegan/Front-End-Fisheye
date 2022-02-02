91532342b455255500edb7d9dec9c400f4d40f66
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-02-02 11:17:38 +0900

    mediaFactories v0

---
+++ scripts/factories/mediaFactories.js
@@ -0,0 +1,21 @@
+function mediaFactories(media) {
+    const { id, photographerId, title, image, video, likes}
+    const photo = `assets/photographers/${image}`;
+    const clip = `assets/photographers/${video}`;
+
+    function cardMedia() {
+
+        const article = document.createElement('article');
+        
+        const title = document.createElement('span');
+        const img = document.createElement('img');
+        img.setAttribute("src", picture);
+        const movie = document.createElement('video');
+        movie.setAttribute("video", video);
+        const likes = document.createElement('span');
+        const heart = document.createElement('span');
+
+        
+        
+    }
+}
