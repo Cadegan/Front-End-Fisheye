989b63571c0fb0482a20142334a64f8e8979ab75
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-03-13 15:30:42 +0900

    LigthBoxTemplate test

---
+++ scripts/factories/templates/lightBoxTemplate.js
@@ -0,0 +1,39 @@
+// Template pour les medias
+export default class LightBoxTemplate {
+    constructor(media) {
+        if (media.image) {
+            return new Image(media)
+        } else if (media.video) {
+            return new Video(media)
+        }
+    }
+}
+
+    //Si c'est une image
+class Image {
+    constructor(media) {
+        this._media = media;
+    }
+
+    createMediaLightbox () {
+        return `
+            
+                <a alt="${this._media.title}" scr="${this._media.image}" data-id="${this._media.id}">
+                <p>alt="${this._media.title}"</p>
+        `
+    }
+}
+
+class Video {
+    constructor(media) {
+        this._media = media;
+    }
+
+    createMediaLightbox() {
+        return `
+            
+                <a scr="${this._media.video}" data-id="${this._media.id}">
+                <p>alt="${this._media.title}"</p>
+        `
+    }
+}
